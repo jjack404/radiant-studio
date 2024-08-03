a60507287d3036bce7fee7c69390caae65caeb1f
dicky <soldicky0@gmail.com>
2024-08-03 02:15:23 -0500

    new repo

--- src/components/Window/Window.jsx
+++ src/components/Window/Window.jsx
@@ -26,7 +26,7 @@ const RadiantsStudio = () => {
       case "Help":
         return <Help />;
       default:
-        return <span>Select a tab to view content</span>;
+        return <span style={{margin: "1em"}}>Select a tab to view content</span>;
     }
   };
 
--- src/components/Window/WindowTabs/Voting.jsx
+++ src/components/Window/WindowTabs/Voting.jsx
@@ -5,27 +5,47 @@ const Voting = () => {
   return (
     <div className={styles.container}>
       <div className={styles.votingGalleryContainer}>
-        <div className={styles.votingGalleryGrid}>
-          <div className={styles.votingTopThree}>
-            <div>1
+        <div className={styles.votingTopThree}>
+          <div className={styles.firstPlace}>
+            <img src={'/assets/ex1.png'} alt="1st place"/>
+            <span>1<sup>st</sup>
+            </span>
+          </div>
+          <div className={styles.secondThird}>
+            <div><img src={'/assets/ex2.png'} alt=""/>
+              <span>2<sup>nd</sup>
+              </span>
             </div>
-            <div>2
-            </div>
-            <div>3
+            <div><img src={'/assets/ex3.png'} alt=""/>
+              <span>3<sup>rd</sup>
+              </span>
             </div>
           </div>
+        </div>
 
-          <div className={styles.votingRemainder}>
-          <div>4
+        <div className={styles.votingRemainder}>
+          <div className={styles.remainderLeft}>
+            <div>4.
             </div>
-            <div>5
+            <div>5.
             </div>
-            <div>6
+            <div>6.
             </div>
+            <div>7.
             </div>
+          </div>
+          <div className={styles.remainderRight}>
+            <div>8.
+            </div>
+            <div>9.
+            </div>
+            <div>10.
+            </div>
+            <div><span>see more</span></div>
+          </div>
         </div>
       </div>
-      </div>);
+    </div>);
 };
 
-      export default Voting;
+export default Voting;
